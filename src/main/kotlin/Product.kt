4e59c7292297058d498ac566819abf89d885326f
Benjamin O'Malley <omalley.benjamin@gmail.com>
2023-02-20 20:08:38 -0500

    passing test for product enum

--- src/main/kotlin/Product.kt
+++ src/main/kotlin/Product.kt
@@ -1,11 +1,11 @@
 enum class Product : IProduct {
   COLA {
-    override fun getValueInCents() = 0
+    override fun getValueInCents() = 100
   },
   CHIPS {
-    override fun getValueInCents() = 0
+    override fun getValueInCents() = 50
   },
   CANDY {
-    override fun getValueInCents() = 0
+    override fun getValueInCents() = 65
   }
 }
